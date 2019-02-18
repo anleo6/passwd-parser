ee18ea781fd714f90d5c08c2adccd3770f8309f3
anleo6 <anleo6@users.noreply.github.com>
2019-02-17 21:41:14 -0800

    Fixed code style according to PEP8

--- passwdparser/__main__.py
+++ passwdparser/__main__.py
@@ -1,61 +1,65 @@
 #! /usr/bin/env python3
 # -*- coding: UTF-8 -*-
- 
+
 import sys
 import argparse
 import json
 import collections
 
-version = "1.0"
+version = "0.1.0"
+
 
 def createParser():
     parser = argparse.ArgumentParser(
-        prog = '''passwd-parser''',
-        description = '''passwd-parser 1.0 - (C) 2019 anleo6. Released under the MIT License. 
-			 This utility parses the UNIX /etc/passwd and /etc/groups files 
-			 and combines the data into a single json output. It is a toy 
-			 treated as a real utility program that could be run in a cron job.''')
+        prog='''passwd-parser''',
+        description='''passwd-parser 1.0 - (C) 2019 anleo6. 
+                    Released under the MIT License.
+                    This utility parses the UNIX /etc/passwd
+                    and /etc/group files and combines the data
+                    into a single json output. 
+                    It is a toy treated as a real utility 
+                    program that could be run in a cron job.''')
 
-    parser.add_argument ('-v', '--version', action ='version',
-        help = 'show version of program',
-        version ='%(prog)s {}'.format(version))
+    parser.add_argument('-v', '--version', action='version',
+        help='show version of program',
+        version='%(prog)s {}'.format(version))
 
-    parser.add_argument('-p', default ='/etc/passwd',
-    	help = 'set the custom path to passwd file, by default /etc/passwd',
-    	metavar = "passwd")
+    parser.add_argument('-p', default='/etc/passwd',
+        help='set the custom path to passwd file, by default /etc/passwd',
+        metavar="passwd")
 
-    parser.add_argument('-g', default ='/etc/group',
-    	help = 'set the custom path to group file, by default /etc/group',
-    	metavar = "group")
+    parser.add_argument('-g', default='/etc/group',
+        help='set the custom path to group file, by default /etc/group',
+        metavar="group")
 
     return parser
 
-def main(args = None):
-    parser = createParser()
-    namespace = parser.parse_args (sys.argv[1:])
 
-# Create temporary dictionary for keep all data to moving it to json format later
+def main(args=None):
+    parser = createParser()
+    namespace = parser.parse_args(sys.argv[1:])
+
     tempDict = {}
 
     try:
-        pswd = open( "{}".format(namespace.p), "r" )
+        pswd = open("{}".format(namespace.p), "r")
 
         for line in pswd:
-            pFields = line.split( ":" )
+            pFields = line.split(":")
             name = pFields[0]
 
-            tempDict[name]      	= collections.OrderedDict()
-            tempDict[name]["uid"]   	= pFields[2]
+            tempDict[name] = collections.OrderedDict()
+            tempDict[name]["uid"] = pFields[2]
             tempDict[name]["full_name"] = pFields[4]
-            tempDict[name]["groups"]    = []
-            
+            tempDict[name]["groups"] = []
+
             try:
-                grp = open( "{}".format(namespace.g), "r" )
+                grp = open("{}".format(namespace.g), "r")
 
                 for line in grp:
-                    gFields = line.split( ":" )
+                    gFields = line.split(":")
                     if gFields[3] != '\n':
-                        gUsers = gFields[3].rstrip().split( "," )
+                        gUsers = gFields[3].rstrip().split(",")
                         for gUser in gUsers:
                             if gUser == name:
                                 tempDict[name]["groups"].append(gFields[0])
@@ -66,12 +70,11 @@ def main(args = None):
 
         pswd.close()
 
-        print(json.dumps(tempDict, indent = 4))
-    
+        print(json.dumps(tempDict, indent=4))
+
     except FileNotFoundError as fnf_error:
         print(fnf_error)
 
- 
-# Entry point 
+# Entry point
 if __name__ == "__main__":
     main()
